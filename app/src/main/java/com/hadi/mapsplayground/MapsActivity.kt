cbb1fd0cdd2583b69249d938dcad29c38f7126bb
Unaisul Hadi <unaisulhadi@gmail.com>
2022-07-12 17:26:52 +0530

    My Location Layer

--- app/src/main/java/com/hadi/mapsplayground/MapsActivity.kt
+++ app/src/main/java/com/hadi/mapsplayground/MapsActivity.kt
@@ -1,29 +1,25 @@
 package com.hadi.mapsplayground
 
-import android.graphics.Color
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.Polyline
-import com.google.android.gms.maps.model.PolylineOptions
 import com.hadi.mapsplayground.databinding.ActivityMapsBinding
 import com.hadi.mapsplayground.misc.CameraAndViewPort
 import com.hadi.mapsplayground.misc.Overlays
-import com.hadi.mapsplayground.misc.Shapes.addCircle
-import com.hadi.mapsplayground.misc.Shapes.addPolygon
-import com.hadi.mapsplayground.misc.Shapes.addPolyline
 import com.hadi.mapsplayground.misc.TypesAndStyle
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -64,10 +60,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-
         // Add a marker in Sydney and move the camera
-
-
         val laMarker = map.addMarker(MarkerOptions()
             .position(losAngeles)
             .title("Marker in LA")
@@ -79,16 +72,47 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // Enable/Disable UI settings for Map
         map.uiSettings.apply {
             isZoomControlsEnabled = true
-        }
-        typesAndStyle.setMapStyle(map, this)
-        //overlays.addGroundOverlay(map)
-        val groundOverlay = overlays.addGroundOverlayBounds(map)
-        lifecycleScope.launch {
-            delay(4000L)
-            //groundOverlay?.remove()
-            groundOverlay?.transparency = 0.5f
+            isMyLocationButtonEnabled = true
         }
 
+        typesAndStyle.setMapStyle(map, this)
+
+        checkLocationPermission()
+    }
+
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        ) {
+            map.isMyLocationEnabled = true
+            Toast.makeText(this, "Already Granted!", Toast.LENGTH_SHORT).show()
+        } else {
+            requestPermission()
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            1)
+    }
+
+    @SuppressLint("MissingPermission", "MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode != 1){
+            return
+        }
+        if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this, "Granted!", Toast.LENGTH_SHORT).show()
+            map.isMyLocationEnabled = true
+        }else{
+            Toast.makeText(this, "We need your permission!", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
