cb00a46169a314a964cbc9ac5882c6f329983039
Unaisul Hadi <unaisulhadi@gmail.com>
2022-07-28 21:26:46 +0530

    Cluster Markers

--- app/src/main/java/com/hadi/mapsplayground/MapsActivity.kt
+++ app/src/main/java/com/hadi/mapsplayground/MapsActivity.kt
@@ -16,9 +16,11 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.Polyline
 import com.google.android.gms.maps.model.PolylineOptions
+import com.google.maps.android.clustering.ClusterManager
 import com.google.maps.android.data.geojson.GeoJsonLayer
 import com.hadi.mapsplayground.databinding.ActivityMapsBinding
 import com.hadi.mapsplayground.misc.CameraAndViewPort
+import com.hadi.mapsplayground.misc.MyItem
 import com.hadi.mapsplayground.misc.Shapes.addCircle
 import com.hadi.mapsplayground.misc.Shapes.addPolygon
 import com.hadi.mapsplayground.misc.Shapes.addPolyline
@@ -38,6 +40,24 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     val newYork = LatLng(40.71392607522911, -73.9915848140515)
     val madrid = LatLng(40.422067989338444, -3.7035171415112678)
     val panama = LatLng(9.092803671276164, -79.53469763577102)
+
+    private val locationList = listOf(
+        LatLng(33.987459169366396, -117.43514666922263),
+        LatLng(34.02844168496524, -116.80892595820784),
+        LatLng(33.987459169366396, -116.01791032324176),
+        LatLng(33.78681578842077, -115.11153826995461),
+        LatLng(33.850708044143765, -114.2546046745436),
+        LatLng(33.58112491982409, -112.71651872998432),
+        LatLng(33.51245201506937, -110.99166521283668),
+        LatLng(33.549084352008315, -110.1621974436902),
+        LatLng(33.484967570542835, -108.95919451960859),
+        LatLng(32.50364494635834, -106.82235368711534),
+    )
+
+    private val titleList = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+    private val snippetList = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+
+    private lateinit var clusterManager: ClusterManager<MyItem>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,14 +86,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         map = googleMap
 
         // Add a marker in Sydney and move the camera
-
-
         val laMarker = map.addMarker(MarkerOptions()
             .position(losAngeles)
             .title("Marker in LA")
             .snippet("This is content")
         )
-
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(losAngeles, 10f))
 
         // Enable/Disable UI settings for Map
@@ -82,6 +99,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
         typesAndStyle.setMapStyle(map, this)
 
+
+        clusterManager = ClusterManager(this, map)
+        map.setOnCameraIdleListener(clusterManager)
+        addMarkers()
+
+    }
+
+    private fun addMarkers() {
+        locationList.zip(titleList).zip(snippetList).forEach { pair ->
+            val myItem = MyItem(pair.first.first, pair.first.second, pair.second)
+            clusterManager.addItem(myItem)
+        }
     }
 
 }
